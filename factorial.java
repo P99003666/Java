225c8b9cf27785b0f40c2a9041b26a4ba73040b8
P99003666 <78848988+P99003666@users.noreply.github.com>
2021-02-24 21:46:16 +0530

    FactorialActivity

--- factorial.java
+++ factorial.java
@@ -1,20 +1,52 @@
+import java.util.Scanner;
 import java.util.*;
-class Arithmatic{
-  static void factorial(int input) {
-	
-	
-}   
+class Arithmetic
+{
+    static int factorial(int n) 
+    { 
+        int f = 1; 
+        for (int i = 1; i <= n; i++) { 
+            f *= i; 
+        } 
+        return f; 
+    } 
+  public static void main (String args[])
+  {
+    int n, temp, digit, count = 0, fact = 1;
+    int nextIndex = 0;
+    int[] intArray = new int[3];
+    Scanner sc = new Scanner (System.in);
 
-  public static void main(String[] args) {
-	int inp1, inp2, inp3;
-	Scanner sc = new Scanner(System.in);
-	 System.out.println("Enter Value of a");
-	 inp1 = sc.nextInt();
-	System.out.println("Enter Value of b");
-	 inp2 = sc.nextInt();
-	System.out.println("Enter Value of Operator");
-	 inp3 = sc.nextInt();
-	performArithmeticOperation(inp1,inp2, inp3);
-   	
-  }
+      System.out.print ("Enter any number: ");
+      n = sc.nextInt ();
+      temp = n;
+
+    while (n > 0)
+      {
+
+	n = n / 10;
+
+	count++;
+      }
+    while (temp > 0)
+      {
+	digit = temp % 10;
+
+	System.out.println (digit);
+	intArray[nextIndex] = digit;
+	++nextIndex;
+	temp = temp / 10;
+	count--;
+      }
+    for (int k = 0; k < intArray.length; k++)
+      {
+	System.out.println (intArray[k]);
+      }
+    for (int i = intArray.length - 1; i >= 0; i--)
+    {
+        
+      System.out.println(intArray[i] + "  ");
+      System.out.println(factorial(intArray[i]));
+}
+}
 }
