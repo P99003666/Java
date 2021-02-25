d7f78b9d0e3653597a60606735daec4d3788ba1c
yshchhipa <78848988+yshchhipa@users.noreply.github.com>
2021-02-25 10:18:03 +0530

    added Validation

--- Activity/higestScore.java
+++ Activity/higestScore.java
@@ -3,7 +3,7 @@ import java.util.*;
 class HighestScore
 {
   static int arry[] = new int[20];
-    static void cricket (int size, int arr[], int score)
+    static void cricket(int size, int arr[], int score)
     {
     for (int i = 1; i<= size; i = i+2)
       {
@@ -14,7 +14,7 @@ class HighestScore
   }
    for (int k = 1; k<= size; k = k+2)
       {
-	    System.out.println(arry[k]);
+	  System.out.println(arry[k]);
   }
   
   
@@ -26,6 +26,12 @@ class HighestScore
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter Number of elements");
     elems = sc.nextInt();
+	if(elems%2!=0)
+	{
+	System.out.println("An even number is required");
+	}
+else
+{
     for(int i =0 ; i<elems ; i++)
     {
         System.out.println("Enter Elem " + i);
@@ -33,6 +39,7 @@ class HighestScore
     }
     compScore = sc.nextInt();
     cricket(elems, intArray, compScore);
-    
+
+    }
   }
 }
